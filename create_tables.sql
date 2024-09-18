157a94bd7832b50e1160e1fcc57d8f71527dd6d7
Liakops <liakops@hotmail.com>
2024-09-18 17:52:16 +0300

    Create tables using scripts

---
+++ create_tables.sql
@@ -0,0 +1,7 @@
+\i scripts/create_calendar.sql
+\i scripts/create_listings.sql
+\i scripts/create_neighbourhoods.sql
+\i scripts/create_geolocation.sql
+\i scripts/create_reviews.sql
+\i scripts/create_listings_summary.sql
+\i scripts/create_reviews_summary.sql
